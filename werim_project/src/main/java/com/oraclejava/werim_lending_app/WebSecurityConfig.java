0c5b40dee55ed8cdbf3db7d367f4cf2071c3cd0e
DESKTOP-NKSGQVR\A <gudtjzzz@naver.com>
2021-04-12 18:03:32 +0900

    한식까지완료

--- werim_project/src/main/java/com/oraclejava/werim_lending_app/WebSecurityConfig.java
+++ werim_project/src/main/java/com/oraclejava/werim_lending_app/WebSecurityConfig.java
@@ -33,6 +33,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception{
 		web.ignoring()
-		    .antMatchers("/resources/**", "/statci/**", "/css/**", "/js/**","/images/**");
+		    .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**","/images/**");
 	}
 }
