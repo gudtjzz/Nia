8135186befc43940f9b992e9177a64e46e663221
DESKTOP-0L72Q1I\OS <gudtjzzz@naver.com>
2021-04-13 11:53:02 +0900

    홈화면/모든 업체선정 완료

--- werim_project/src/main/java/com/oraclejava/werim_lending_app/controller/HomeController.java
+++ werim_project/src/main/java/com/oraclejava/werim_lending_app/controller/HomeController.java
@@ -41,4 +41,39 @@ public class HomeController {
 		mav.setViewName("koreanfood");
 		return mav;
 	}
+	
+	@RequestMapping(value = "/japanesefood", method = RequestMethod.GET)
+	public ModelAndView japanesefood() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("japanesefood");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/jokbo", method = RequestMethod.GET)
+	public ModelAndView jokbo() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("jokbo");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/yasik", method = RequestMethod.GET)
+	public ModelAndView yasik() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("yasik");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/boonsik", method = RequestMethod.GET)
+	public ModelAndView boonsik() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("boonsik");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/cafe", method = RequestMethod.GET)
+	public ModelAndView cafe() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("cafe");
+		return mav;
+	}
 }
