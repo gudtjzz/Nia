efae08f397aa799f2c74643e6130813e10c9aefc
DESKTOP-NKSGQVR\A <joesunik@naver.com>
2021-04-19 18:29:47 +0900

    userLayout으로 store 컨트롤러 내용 이동

--- werim_project/src/main/java/com/oraclejava/werim_lending_app/controller/StoreController.java
+++ werim_project/src/main/java/com/oraclejava/werim_lending_app/controller/StoreController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.oraclejava.werim_lending_app.CustomUser;
 import com.oraclejava.werim_lending_app.dao.FranchiseRepository;
@@ -32,8 +33,10 @@ public class StoreController {
 	private StoreRepository storeRepository;
 	
 	@GetMapping("/request")
-	public String requestStore() {
-		return "/store/request";
+	public ModelAndView requestStore(ModelAndView mav) {
+		mav.setViewName("/user/userLayout");
+		mav.addObject("contents", "/user/store/request :: store-request-page");
+		return mav;
 	}
 	
 	@PostMapping("/request")
@@ -47,17 +50,19 @@ public class StoreController {
 	}
 	
 	@GetMapping("/register")
-	public String register(Model model, @AuthenticationPrincipal CustomUser user) {
-		if( !model.containsAttribute("store")) {
+	public ModelAndView register(ModelAndView mav, @AuthenticationPrincipal CustomUser user) {
+		if( !mav.getModelMap().containsAttribute("store")) {
 			Store store = new Store();
 			store.setUser_id(user.getUserinfo().getUser_id());
-			model.addAttribute("store", store );
+			mav.addObject("store", store );
 		}
 		List<Franchise> fran_list = (List<Franchise>) franchiseRepository.findAll();
-		model.addAttribute("fran_list",fran_list);
-		model.addAttribute("userinfo", user.getUserinfo());
+		mav.addObject("fran_list",fran_list);
+		mav.addObject("userinfo", user.getUserinfo());
+		mav.addObject("contents", "user/store/register :: store-register-page");
+		mav.setViewName("/user/store/register");
 		
-		return "/store/register";
+		return mav;
 	}
 	
 	@PostMapping("/register")
