21996ab0c16cb27a21c1250b48d4f487044831b6
DESKTOP-RQUS2IL\yaung <yaungjun3@naver.com>
2021-04-20 15:49:00 +0900

    카테고리 수정 홈 레이아웃 틀 제작 틀안에 메뉴나오게 설정

--- werim_project/src/main/java/com/oraclejava/werim_lending_app/controller/StoreController.java
+++ werim_project/src/main/java/com/oraclejava/werim_lending_app/controller/StoreController.java
@@ -1,21 +1,15 @@
 package com.oraclejava.werim_lending_app.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.oraclejava.werim_lending_app.CustomUser;
-import com.oraclejava.werim_lending_app.dao.FranchiseRepository;
-import com.oraclejava.werim_lending_app.dao.StoreRepository;
 import com.oraclejava.werim_lending_app.dao.UserInfoRepository;
-import com.oraclejava.werim_lending_app.dto.Franchise;
-import com.oraclejava.werim_lending_app.dto.Store;
 import com.oraclejava.werim_lending_app.dto.UserInfo;
 
 @Controller
@@ -25,12 +19,6 @@ public class StoreController {
 	@Autowired
 	private UserInfoRepository userinfoRepository;
 	
-	@Autowired
-	private FranchiseRepository franchiseRepository;
-	
-	@Autowired
-	private StoreRepository storeRepository;
-	
 	@GetMapping("/request")
 	public String requestStore() {
 		return "/store/request";
@@ -38,6 +26,7 @@ public class StoreController {
 	
 	@PostMapping("/request")
 	public String requestStore(@AuthenticationPrincipal CustomUser user) {
+		System.out.println("post");
 		UserInfo userinfo = user.getUserinfo();
 		userinfo.setStore_state("1");
 		
@@ -47,22 +36,7 @@ public class StoreController {
 	}
 	
 	@GetMapping("/register")
-	public String register(Model model, @AuthenticationPrincipal CustomUser user) {
-		if( !model.containsAttribute("store")) {
-			Store store = new Store();
-			store.setUser_id(user.getUserinfo().getUser_id());
-			model.addAttribute("store", store );
-		}
-		List<Franchise> fran_list = (List<Franchise>) franchiseRepository.findAll();
-		model.addAttribute("fran_list",fran_list);
-		model.addAttribute("userinfo", user.getUserinfo());
-		
+	public String register() {
 		return "/store/register";
-	}
-	
-	@PostMapping("/register")
-	public String register(Store store) {
-		storeRepository.save(store);
-		return "redirect:/user/userLayout";
 	}
 }
